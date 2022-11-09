6a3bc01fa6093d705f4d25846ca6d180c3ede7d6
hoangkm13 <hoang.workingemail@gmail.com>
2022-11-09 14:48:10 +0700

    CRUD Address

--- DATN.Web.Api/Controllers/AddressController.cs
+++ DATN.Web.Api/Controllers/AddressController.cs
@@ -1,11 +1,11 @@
-﻿using DATN.Web.Service.Interfaces.Repo;
+﻿using System;
+using System.Threading.Tasks;
+using DATN.Web.Service.DtoEdit;
+using DATN.Web.Service.Interfaces.Repo;
 using DATN.Web.Service.Interfaces.Service;
 using DATN.Web.Service.Model;
+using DATN.Web.Service.Properties;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DATN.Web.Api.Controllers
 {
@@ -13,7 +13,7 @@ namespace DATN.Web.Api.Controllers
     /// Controller Đại chỉ
     /// </summary>
     /// CreatedBy: dqdat (20/07/2021)
-    [Route("api/[controller]s")]
+    [Route("api/[controller]es")]
     public class AddressController : BaseController<AddressEntity>
     {
         /// <summary>
@@ -25,15 +25,102 @@ namespace DATN.Web.Api.Controllers
         /// Repo Đại chỉ
         /// </summary>
         IAddressRepo _addressRepo;
+
         /// <summary>
         /// Hàm khởi tạo
         /// </summary>
         /// <param name="AddressService"></param>
         /// <param name="AddressRepo"></param>
-        public AddressController(IAddressService addressService, IAddressRepo addressRepo, IServiceProvider serviceProvider) : base(addressService, addressRepo, serviceProvider)
+        public AddressController(IAddressService addressService, IAddressRepo addressRepo,
+            IServiceProvider serviceProvider) : base(addressService, addressRepo, serviceProvider)
         {
             _addressService = addressService;
             _addressRepo = addressRepo;
+        }
+
+        /// <summary>
+        /// Create An Address Of User
+        /// </summary>
+        /// <param name="createAddress">CreareAddress</param>
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateAddress([FromBody] CreateAddress createAddress)
+        {
+            try
+            {
+                var res = await _addressService.CreateAddress(createAddress);
+                if (res != null)
+                {
+                    var actionResult = new DAResult(200, Resources.getDataSuccess, "", res);
+                    return Ok(actionResult);
+                }
+                else
+                {
+                    var actionResult = new DAResult(204, Resources.noReturnData, "", null);
+                    return Ok(actionResult);
+                }
+            }
+            catch (Exception exception)
+            {
+                var actionResult = new DAResult(500, Resources.error, exception.Message, null);
+                return Ok(actionResult);
+            }
+        }
+        
+        /// <summary>
+        /// Update An Address Of User
+        /// </summary>
+        /// <param name="updateAddress">UpdateAddress</param>
+        /// <param name="address_id">address_id</param>
+        [HttpPut("update/{address_id}")]
+        public async Task<IActionResult> UpdateAddress([FromBody] UpdateAddress updateAddress, Guid address_id)
+        {
+            try
+            {
+                var res = await _addressService.UpdateAddress(updateAddress, address_id);
+                if (res != null)
+                {
+                    var actionResult = new DAResult(200, Resources.getDataSuccess, "", res);
+                    return Ok(actionResult);
+                }
+                else
+                {
+                    var actionResult = new DAResult(204, Resources.noReturnData, "", null);
+                    return Ok(actionResult);
+                }
+            }
+            catch (Exception exception)
+            {
+                var actionResult = new DAResult(500, Resources.error, exception.Message, null);
+                return Ok(actionResult);
+            }
+        }
+        
+        /// <summary>
+        /// Update An Address Of User
+        /// </summary>
+        /// <param name="address_id">address_id</param>
+        [HttpDelete("delete/{address_id}")]
+        public async Task<IActionResult> DeleteAddress(Guid address_id)
+        {
+            try
+            {
+                var res = await _addressService.DeleteAddress(address_id);
+                if (res != null)
+                {
+                    var actionResult = new DAResult(200, Resources.getDataSuccess, "", res);
+                    return Ok(actionResult);
+                }
+                else
+                {
+                    var actionResult = new DAResult(204, Resources.noReturnData, "", null);
+                    return Ok(actionResult);
+                }
+            }
+            catch (Exception exception)
+            {
+                var actionResult = new DAResult(500, Resources.error, exception.Message, null);
+                return Ok(actionResult);
+            }
         }
     }
 }
