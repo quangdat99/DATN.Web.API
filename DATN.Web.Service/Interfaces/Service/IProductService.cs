36f8bdb43dcf5054f750ea834d076008a12bb5bd
hoangkm13 <hoang.workingemail@gmail.com>
2022-11-15 16:07:44 +0700

    api Get List Product, Delete Product Detail, Update Product Detail, Add Single Product To Cart

--- DATN.Web.Service/Interfaces/Service/IProductService.cs
+++ DATN.Web.Service/Interfaces/Service/IProductService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using DATN.Web.Service.DtoEdit;
 using DATN.Web.Service.Model;
 
 namespace DATN.Web.Service.Interfaces.Service
@@ -15,5 +16,27 @@ namespace DATN.Web.Service.Interfaces.Service
         /// </summary>
         /// <param name="cart_id">Cart Id</param>
         Task<List<ProductEntity>> GetListProductFromCart(Guid cart_id);
+
+        /// <summary>
+        /// Delete Product Detail
+        /// </summary>
+        /// <param name="productDetailId">Product Detail</param>
+        Task<ProductDetailEntity> DeleteProductDetail(Guid productDetailId);
+
+        /// <summary>
+        /// Customer Update Product Detail In Cart
+        /// </summary>
+        /// <param name="productDetailId">Product Detail Id</param>
+        /// <param name="customerUpdateProductDetail">Update Info</param>
+        Task<ProductDetailEntity> CustomerUpdateProductDetail(Guid productDetailId,
+            CustomerUpdateProductDetail customerUpdateProductDetail);
+
+        /// <summary>
+        /// Add Single Product ToCart
+        /// </summary>
+        /// <param name="cartId">Cart ID</param>
+        /// <param name="newProductId">New Product Id</param>
+        Task<List<ProductEntity>> AddSingleProductToCart(Guid cartId,
+            Guid newProductId);
     }
 }
